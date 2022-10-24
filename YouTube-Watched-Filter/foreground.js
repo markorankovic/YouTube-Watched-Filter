247122750b6dc37a570f30a2e47ba0f92539da3c
Marko Ranković <marko@markorankovic.com>
2022-10-24 19:50:39 +0100

    Unwatched videos can now be filtered out instead

--- YouTube-Watched-Filter/foreground.js
+++ YouTube-Watched-Filter/foreground.js
@@ -1,6 +1,15 @@
 console.log('foreground.js executing')
 
-var storedVideos = ['s:0oqOUhFaToQ', 'Ri1CNMzydvg', 'wurRL7HM9Xo'] // TODO: Replace this with the actual storage
+var storedVideos = ['s:0oqOUhFaToQ', 'Ri1CNMzydvg', 'wurRL7HM9Xo', 'InFi80Cf4PQ'] // TODO: Replace this with the actual storage
+const reversed = false
+
+function videosSub(lhs, rhs) {
+    var res = []
+    for (e of lhs) {
+        if (!rhs.includes(e)) res.push(e)
+    }
+    return res
+}
 
 function getVideosWithMatchingIds(videosLoaded, videosToFilter) {
     var videosToRemove = []
@@ -9,7 +18,10 @@ function getVideosWithMatchingIds(videosLoaded, videosToFilter) {
             if (videoElementToVideoId(videoLoaded) == videoToFilter) videosToRemove.push(videoLoaded)
         }
     }
-    return videosToRemove
+    const reversedVideosToRemove = videosSub(videosLoaded, videosToRemove)
+    const reversedVideosToRemoveInIds = reversedVideosToRemove.map(e => videoElementToVideoId(e))
+    //console.log('reversedVideosToRemoveInIds: ', reversedVideosToRemoveInIds)
+    return reversed ? reversedVideosToRemove : videosToRemove
 }
 
 function removeVideosExistingInFilter(videosLoaded) {
